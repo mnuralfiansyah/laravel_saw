ea33d0179bbf2a876e623ad8ae3dc73f1bb93deb
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2018-08-13 15:10:21 +0800

    bismillah

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
